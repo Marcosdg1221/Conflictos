9fa259246f81cf0d5bf84bf3f7017d95c3c970ba
mdominguez <mdominguez@106-28.info2>
2022-12-13 11:49:04 +0100

    Commit 3

--- ResolucionConflictos/src/paquete/Main.java
+++ ResolucionConflictos/src/paquete/Main.java
@@ -6,6 +6,6 @@ public class Main {
 		
 	public static void main(String[] args) {
 		
-		System.out.println("Hola, soy Isabel Katharina Loerzer");
+		System.out.println("Hola, soy Marcos Domínguez");
 	}
 }
